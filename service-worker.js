e1fdc3ac076860dc8941a36b896f083fd531f085
Hub-bot24 <jdhugoaus@gmail.com>
2025-09-10 21:53:41 +1000

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='mattest-cache-v23';
+const CACHE = 'mattest-cache-v24';
 const ASSETS=['./','./index.html','./app-legacy.js?v=23','./manifest.webmanifest','./icons/icon-192.png','./icons/icon-512.png'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting();});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
