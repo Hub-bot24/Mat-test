561d0242d9cf478f7a753c4c0a5b1a23e3b2b00f
Hub-bot24 <jdhugoaus@gmail.com>
2025-09-16 12:37:46 +1000

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,38 @@
-// service worker placeholder
+const CACHE_NAME = 'mat-test-v202pro-' + Date.now();
+const FILES_TO_CACHE = [
+  './',
+  './index.html',
+  './app.js',
+  './manifest.webmanifest',
+  './service-worker.js'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keyList) =>
+      Promise.all(keyList.map((key) => {
+        if (key !== CACHE_NAME) {
+          return caches.delete(key);
+        }
+      }))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
