c9715ec4e3d73d1b20835f576f388a1a15f1fec3
Hub-bot24 <jdhugoaus@gmail.com>
2025-09-17 10:10:07 +1000

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,38 +1,25 @@
-const CACHE_NAME = 'mat-test-v202pro-' + Date.now();
-const FILES_TO_CACHE = [
+
+const CACHE = 'mattest-cache-v31';
+const ASSETS = [
   './',
-  './index.html',
-  './app.js',
-  './manifest.webmanifest',
-  './service-worker.js'
+  './index.html?v=31',
+  './style.css?v=31',
+  './manifest.webmanifest?v=31',
+  './icons/icon-192.png',
+  './icons/icon-512.png'
 ];
 
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(FILES_TO_CACHE);
-    })
-  );
-  self.skipWaiting();
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
 });
 
-self.addEventListener('activate', (event) => {
-  event.waitUntil(
-    caches.keys().then((keyList) =>
-      Promise.all(keyList.map((key) => {
-        if (key !== CACHE_NAME) {
-          return caches.delete(key);
-        }
-      }))
-    )
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))) // drop old
+    .then(() => self.clients.claim())
   );
-  self.clients.claim();
 });
 
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
-  );
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
 });
